46df52a8b492f4753641c9fa0fd045aad9d0be91
mihua2 <michael.huang1@ingka.ikea.com>
2021-08-05 11:53:16 +0800

    add api header

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/sh777/simple-service-test-duration
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/viper v1.8.1
+)
--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
+
+var Config *viper.Viper
+
+func init() {
+	Config = viper.New()
+}
 
 func main() {
 	hostName, err := os.Hostname()
@@ -15,6 +22,14 @@ func main() {
 		panic(err)
 	}
 
+	// Configurations from environment variables
+	Config.SetEnvPrefix("SIMPLE_SERVICE")
+	Config.AutomaticEnv()
+
+	service_tag := Config.GetString("tag")
+	if service_tag == "" {
+		service_tag = "service"
+	}
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -26,7 +41,11 @@ func main() {
 		duration := c.Param("duration")
 		d, _ := strconv.Atoi(duration)
 		time.Sleep(time.Duration(d) * time.Second)
-		c.String(http.StatusOK, "v4 delayed %s seconds on %s", duration, hostName)
+		c.String(http.StatusOK, "%s delayed %s seconds on %s", service_tag, duration, hostName)
+	})
+
+	router.GET("/header", func(c *gin.Context) {
+		c.JSON(http.StatusOK, c.Request.Header)
 	})
 
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
